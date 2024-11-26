7c2af43af877a832ec23107409f4f97238bdfb12
unknown <oriex21001@mail.unpad.ac.id>
2024-11-26 15:19:03 +0700

    udah yang keberapa yaa pantek

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->group('web', [
-            \App\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\TrustProxies::class,
             \Illuminate\Http\Middleware\HandleCors::class,
-            \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
             \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-            \App\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
             \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
             
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
